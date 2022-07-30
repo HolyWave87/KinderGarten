e0241e3c0c31519ab6efdd2c60d52d5e04c7bab3
GrigorevDmitriy <107036333+HolyWave87@users.noreply.github.com>
2022-07-30 13:58:56 +0300

    update - change form, add contacts, map, carousel

--- Site/sendMail.php
+++ Site/sendMail.php
@@ -4,6 +4,6 @@ $tel = $_POST['tel'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 // echo "Привет $userName", $tel, $email, $message ;
-mail("holywave@yandex.ru", "Тестовое письмо", "name: $userName, tel: $tel, email: $email, message: $message");
+mail("holywave@yandex.ru", "Письмо с сайта детского сада", "Имя: $userName, Телефон: $tel, Email: $email, Сообщение: $message");
 
 // header("Location: index.html");
